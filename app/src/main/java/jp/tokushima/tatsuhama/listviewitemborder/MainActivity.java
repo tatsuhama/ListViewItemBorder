664194b0fb09efe897373701b5069d7255cb8cfa
tatsuhama <kenichi.tatsuhama@gmail.com>
2015-12-31 11:46:58 +0900

    再現パターン

--- app/src/main/java/jp/tokushima/tatsuhama/listviewitemborder/MainActivity.java
+++ app/src/main/java/jp/tokushima/tatsuhama/listviewitemborder/MainActivity.java
@@ -2,6 +2,7 @@ package jp.tokushima.tatsuhama.listviewitemborder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView list = (ListView) findViewById(R.id.list_view);
+        list.setAdapter(createAdapter());
+    }
+
+    private MyAdapter createAdapter() {
+        MyAdapter adapter = new MyAdapter(this);
+        for (int i = 0; i < 30; i++) {
+            adapter.add(i % 6 < 3);
+        }
+        return adapter;
     }
 }
---
+++ app/src/main/java/jp/tokushima/tatsuhama/listviewitemborder/MyAdapter.java
@@ -0,0 +1,44 @@
+package jp.tokushima.tatsuhama.listviewitemborder;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by tatsuhama on 15/12/31.
+ */
+public class MyAdapter extends ArrayAdapter<Boolean> {
+    public MyAdapter(Context context) {
+        super(context, 0);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final ViewHolder holder;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        }
+        holder.text.setText(getItem(position) ? "Enabled" : "Disabled");
+        return convertView;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return getItem(position);
+    }
+
+    static class ViewHolder {
+        TextView text;
+
+        ViewHolder(View view) {
+            this.text = (TextView) view.findViewById(R.id.list_item_text);
+        }
+    }
+}
